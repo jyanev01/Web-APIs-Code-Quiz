7bad99627bd007eabff495391c092d5e2143bb29
jyanev01 <jordanyanev@gmail.com>
2021-09-19 21:38:28 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,39 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start');
+
+// Timer that counts down from 5
+function countdown() {
+    var timeLeft = 75;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function() {
+      // alert("One second has passed")
+      if (timeLeft >1) {
+        
+        timerEl.textContent= timeLeft + " seconds remaining";  
+        }else {
+          timerEl.textContent= timeLeft + " second remaining";
+        } 
+  
+        if(timeLeft === 0){
+          clearInterval(timeInterval);
+          timerEl.textContent=""
+          displayMessage();
+        }
+        timeLeft--;
+  
+    }, 1000);
+    
+};
+
+// 
+  
 // TODO: Create an array with five question objects
 var questions=[
-    { q:"skye blue?", a: true},
-    { q:"bananas are vegetables?", a: false},
-    { q:"delcaration of indepenced was 1745", a: false},
+    { q:"Is the skye blue?", a: true},
+    { q:"Are bananas are vegetables?", a: false},
+    { q:"Was the delcaration of indepenced written in 1745?", a: false},
     { q:"365 days in a year", a: true},
     { q:"42 ounces in a pound", a: false},
 ];
@@ -13,17 +44,17 @@ var questions=[
 var score=0
 
 // TODO: Iterate over the questions array and display each question in a confirmation box
-for (let index = 0; index < questions.length; index++); {
+for (let index = 0; index < questions.length; index++) {
     
-    var userAnswer = confrim(questions[index].q);
+    var userAnswer = confirm(questions[index].q);
 
-    if(userAnswer === questions[index].q) {
+    if(userAnswer === questions[index].a) {
         score++;
         alert("Correct");
     } else {
         alert("Incorrect");
-    }
-}
+    };
+};
 
 alert("Final Score is "+ score + "/"+questions.length);
 
@@ -33,3 +64,10 @@ alert("Final Score is "+ score + "/"+questions.length);
 // TODO: Alert the user if they are correct or wrong. Increment the score accordingly
 
 // TODO: At the end of the game, alert the user with the final score
+
+
+
+// Displays the message one word at a time
+
+
+startBtn.onclick = countdown;
